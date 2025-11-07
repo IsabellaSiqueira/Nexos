d16d3957353908b4477382eff3bd8cc0f8621857
ÏsabellaSiqueira <isabella.tragante@hotmail.com>
2025-11-07 20:06:14 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    // ESSENCIAL: Diz ao Tailwind onde escanear as classes (incluindo as classes de colchetes)
+    content: [
+        "./*.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
